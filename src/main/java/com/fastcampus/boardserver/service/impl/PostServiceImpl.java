91db422b4e3f6ca825e2201c54c357029596d146
beckhem96 <beckhem96@naver.com>
2025-05-14 00:19:56 +0900

    게시판 마무리 추가

---
+++ src/main/java/com/fastcampus/boardserver/service/impl/PostServiceImpl.java
@@ -0,0 +1,64 @@
+package com.fastcampus.boardserver.service.impl;
+
+import com.fastcampus.boardserver.dto.PostDTO;
+import com.fastcampus.boardserver.dto.UserDTO;
+import com.fastcampus.boardserver.mapper.PostMapper;
+import com.fastcampus.boardserver.mapper.UserProfileMapper;
+import com.fastcampus.boardserver.service.PostService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Log4j2
+public class PostServiceImpl implements PostService {
+    @Autowired
+    private PostMapper postMapper;
+    @Autowired
+    private UserProfileMapper userProfileMapper;
+
+    @Override
+    public void register(String id, PostDTO postDTO) {
+        log.error("lwt check {}", id);
+        UserDTO memberInfo = userProfileMapper.getUserProfile(id);
+        postDTO.setUserId(memberInfo.getId());
+        postDTO.setCreateTime(new Date());
+
+        if (memberInfo != null) {
+            postMapper.register(postDTO);
+        } else {
+            log.error("register error, {}", postDTO);
+            throw new RuntimeException("register error, " + postDTO);
+        }
+    }
+
+    @Override
+    public List<PostDTO> getMyPosts(int accountId) {
+        List<PostDTO> postDTOList = postMapper.selectMyPosts(accountId);
+        return postDTOList;
+    }
+
+    @Override
+    public void updatePosts(PostDTO postDTO) {
+        if (postDTO != null && postDTO.getId() != 0) {
+            postMapper.updatePosts(postDTO);
+        } else {
+            log.error("updatePosts error, {}", postDTO);
+            throw new RuntimeException("updatePosts error, " + postDTO);
+        }
+    }
+
+    @Override
+    public void deletePosts(int userId, int postId) {
+        if (userId != 0 && postId != 0) {
+            postMapper.deletePosts(postId);
+        } else {
+            log.error("deletePosts error, {}", postId);
+            throw new RuntimeException("deletePosts error, " + postId);
+        }
+    }
+}
