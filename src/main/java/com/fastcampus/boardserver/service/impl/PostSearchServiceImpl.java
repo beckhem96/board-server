968d332f4b787b12022e1aeaec187f9e75f6551d
beckhem96 <beckhem96@naver.com>
2025-05-13 22:57:10 +0900

    백업

---
+++ src/main/java/com/fastcampus/boardserver/service/impl/PostSearchServiceImpl.java
@@ -0,0 +1,23 @@
+package com.fastcampus.boardserver.service.impl;
+
+import com.fastcampus.boardserver.dto.PostDTO;
+import com.fastcampus.boardserver.dto.request.PostSearchRequest;
+import com.fastcampus.boardserver.service.PostSearchService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Log4j2
+public class PostSearchServiceImpl implements PostSearchService {
+
+//    @Autowired
+//    private PostSearchMapper
+
+    @Override
+    public List<PostDTO> getPosts(PostSearchRequest postSearchRequest) {
+        return List.of();
+    }
+}
